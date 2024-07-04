30a5e8cc94c082afbddc24322e45a0f5ad7e93df
ayushRajar <theayushraj.ar@outlook.com>
2024-07-04 11:59:00 +0530

    AuthRouter Version 1

---
+++ Routers/authRouter.js
@@ -0,0 +1,11 @@
+const {Router} = require ("express");
+const Router=Router();
+
+router.get('\signup',() => {});
+router.post('\signup',()=> {});
+router.post('\signup',()=> {});
+router.post('\signup',()=> {});
+
+module.exports = router;
+
+
